8fecab5ab87cf03a808a12775a6441b0ab081e3f
diegomoise1 <diegomoises1@hotmail.com>
2025-02-15 00:50:02 -0600

    Update create_standalone_steamlink.sh

--- resources/build/rpi/create_standalone_steamlink.sh
+++ resources/build/rpi/create_standalone_steamlink.sh
@@ -34,6 +34,7 @@ if [ -f "$EXTRADEPS" ]; then
 fi
 
 # Include dependencies not present in LibreELEC
+# Include dependencies not present in LibreELEC
 DEPENDENCIES="
   libGL.so*
   libGLX.so*
@@ -57,7 +58,69 @@ DEPENDENCIES="
   libssl.so*
   libwayland-client.so*
   libxcb.so*
+  libavcodec.so*
+  libepoxy.so*
+  libwayland-egl.so*
+  libvpx.so*
+  libwebpmux.so*
+  libwebp.so*
+  liblzma.so*
+  libdav1d.so*
+  librsvg-2.so*
+  libzvbi.so*
+  libsnappy.so*
+  libaom.so*
+  libcodec2.so*
+  libgsm.so*
+  libjxl.so*
+  libjxl_threads.so.0.7*
+  libmp3lame.so*
+  libopenjp2.so*
+  librav1e.so*
+  libshine.so*
+  libSvtAv1Enc.so*
+  libtheoraenc.so*
+  libtheoradec.so*
+  libtwolame.so*
+  libvorbis.*
+  libvorbisenc.so*
+  libx264.so*
+  libx265.so*
+  libxvidcore.so*
+  libva.so*
+  libva-drm.so*
+  libva-x11.so*
+  libvdpau.so*
+  libOpenCL.so*
+  libmd4c.so*
+  libgssapi_krb5.so*
+  libdouble-conversion.so*
+  libzstd.so*
+  libgdk_pixbuf-2.0.so*
+  libpangocairo-1.0.so*
+  libpango-1.0.so*
+  libbrotlidec.so*
+  libhwy.so*
+  libbrotlienc.so*
+  liblcms2.so*
+  libogg.so*
+  libnuma.so*
+  libXfixes.so*
+  libX11-xcb.so*
+  libxcb-dri3.so*
+  libkrb5.so*
+  libk5crypto.so*
+  libcom_err.so*
+  libkrb5support.so*
+  libpangoft2-1.0.so*
+  libfribidi.so*
+  libthai.so*
+  libbrotlicommon.so*
+  libkeyutils.so*
+  libdatrie.so*
+  libmd.so*
 "
+
 
 for DEP in $DEPENDENCIES; do
   cp --verbose --no-dereference --recursive /usr/lib/arm-linux-gnueabihf/$DEP /tmp/steamlink/lib/
